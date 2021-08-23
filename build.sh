9a561ab0eef0ca8c6d20d641ae5585f31f5f3a85
Ryan Gartin <ryan@eagledevelopers.net>
2021-08-23 16:41:23 -0400

    Updates

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 set -e
+set -x
 
 if [ -z "${IMAGE_NAME}" ]; then
   echo "Missing required ENV variables; Exiting." && exit 1
@@ -8,23 +9,25 @@ fi
 EXTRA_BUILD_ARGS=${EXTRA_BUILD_ARGS:-""}
 DOCKERFILE_DIRECTORY=${DOCKERFILE_DIRECTORY:-"./"}
 DOCKERFILE_NAME=${DOCKERFILE_NAME:-"Dockerfile"}
-ARTIFACT_PATHS=${ARTIFACT_PATHS:-"/test-results"}
-CACHED_TARGETS=${CACHED_TARGETS:-"tooling,dependencies"}
+ARTIFACT_PATHS=${ARTIFACT_PATHS:-"/test-results,/reports,/build_status"}
+CACHED_STAGES=${CACHED_STAGES:-"tooling,runtime,dependencies"}
+BUILD_STAGE=${BUILD_STAGE:-"build"}
+RELEASE_STAGE=${RELEASE_STAGE:-$IMAGE_NAME}
 PULL_IMAGES=${PULL_IMAGES:-""}
 CACHE_LAYER_PREFIX=${CACHE_LAYER_PREFIX:-$IMAGE_NAME}
-RELEASE_TARGET=${RELEASE_TARGET:-$IMAGE_NAME}
+BUILD_IMAGE_NAME="${CACHE_LAYER_PREFIX}_build"
 SEMVER=${SEMVER:-1.0.0}
 dockerfile_directory=${DOCKERFILE_DIRECTORY:-./}
 dockerfile_name=${DOCKERFILE_NAME:-Dockerfile}
 DOCKERFILE="${dockerfile_directory%/}/${dockerfile_name}"
-CACHED_TARGETS=${CACHED_TARGETS:-tooling,dependencies,runtime}
 GIT_BRANCH=${GITHUB_REF}
 GIT_SHA_SHORT=$(git rev-parse --short HEAD)
-LOCAL_IMAGE_TAG="${IMAGE_NAME}:${SEMVER}_${GIT_SHA_SHORT}"
+RELEASE_TAG=${RELEASE_TAG:-"release"}
+IMAGE_TAG="${IMAGE_NAME}:${SEMVER}_${RELEASE_TAG}.${GIT_SHA_SHORT}"
 echo "Dockerfile: ${DOCKERFILE}"
 echo "GIT_BRANCH: ${GIT_BRANCH}"
 echo "Git commit hash: ${GIT_SHA_SHORT}"
-echo "Targets to cache: ${CACHED_TARGETS}"
+echo "Targets to cache: ${CACHED_STAGES}"
 echo "build_args: ${build_args}"
 echo "semver: ${SEMVER}"
 # Validate SEMVER
@@ -32,59 +35,85 @@ semver diff ${SEMVER} ${SEMVER}
 # Save ENV variables for downstream Github Action steps
 echo "GIT_SHA_SHORT=${GIT_SHA_SHORT}" >> $GITHUB_ENV
 echo "ESCAPEDSEMVER=${SEMVER//[-+]/_}" >> $GITHUB_ENV
-echo "LOCAL_IMAGE_TAG=${LOCAL_IMAGE_TAG}" >> $GITHUB_ENV
+echo "IMAGE_TAG=${IMAGE_TAG}" >> $GITHUB_ENV
 
-build_target() {
-  _target=${1}
-  _tag=${2}
-  command="DOCKER_BUILDKIT=1 docker build --pull "
-  command+="--target \"${_target}\" "
-  if [ ! -z "${_tag}" ]; then
-    # Final build stage only
-    echo "Final build target: ${_target}"
-    command+="-t \"${_tag}\" "
-    command+="--squash "
-  elif [[ $CACHED_TARGETS == *"${_target}"* ]]; then
-    echo "Caching target: ${_target}"
-    command+="-t \"${CACHE_LAYER_PREFIX}_${_target}\" "
-  elif [[ "$_target" == *"report"* ]]; then
-    echo "Extracting test results to ./build"
-    command+="--output ./build "
-  fi
-  command+="--build-arg SEMVER=\"${SEMVER}\" ${EXTRA_BUILD_ARGS} "
-  command+="-f ${DOCKERFILE} ./ "
-  echo $command
-  eval $command
+export DOCKER_BUILDKIT=1
+
+build_stage() {
+  _stage=${1}
+  echo "Building stage: ${_stage}"
+  docker build ${stage_prefix}  \
+    --target ${_stage} \
+    -t "${CACHE_LAYER_PREFIX}_${_stage}" \
+    --build-arg SEMVER="${SEMVER}" \
+    ${EXTRA_BUILD_ARGS} \
+    -f ${DOCKERFILE} \
+    ./
 }
 
+build_final() {
+  _stage=${1}
+  _tag=${2}
+  echo "Building final stage: ${_stage}"
+  docker build \
+    --squash \
+    -t "${_tag}" \
+    --target ${_stage} \
+    --build-arg SEMVER="${SEMVER}" \
+    ${EXTRA_BUILD_ARGS} \
+    -f ${DOCKERFILE} \
+    ./
+}
+
+export_reports() {
+  id=$(docker create ${BUILD_IMAGE_NAME})
+  for _path in ${ARTIFACT_PATHS//,/ }; do
+    docker cp $id:/${_path} ./build/${_path} || true
+  done
+  docker rm $id
+}
+
+on_exit() {
+  ret_code=$?
+  docker rmi --force ${BUILD_IMAGE_NAME}
+  exit $ret_code
+}
 
 ###############
 # Warmup
 ###############
 
-for _ in {1..12}; do
-  [[ "$(ps aux | grep -c '[d]ocker system prune\|[d]ocker rmi')" -ne "0" ]] && echo "waiting for docker image cleanup to finish before starting the build" && sleep 10
-done
-
 # Refresh Images
 echo "Pulling latest images..."
 for _image in ${PULL_IMAGES//,/ }; do
-    docker pull "${_image}"
+  docker pull "${_image}"
 done
 
 ###############
 # Build
 ###############
 
-
-stages=($(cat ${DOCKERFILE} | grep -o 'as [a-z-]*' | sed 's/as //'));
-for stage in ${stages[@]}; do
-  if [[ $stage == "${RELEASE_TARGET}" ]]; then
-    continue
+# Building persistent stages
+for stage in ${CACHED_STAGES//,/ }; do
+  if grep -i -q "as ${stage}\$" "${DOCKERFILE}"; then
+    build_stage $stage
   fi
-  echo "Building Stage: '${stage}'"
-  build_target ${stage}
 done
 
+# Build
+build_stage ${BUILD_STAGE}
+trap "on_exit" EXIT
+
+# Export tests
+export_reports
+
+status_file=./build/build_status
+if [ -f "${status_file}" ]; then
+  status=$(cat ${status_file})
+  if [ ${status} -ne 0 ]; then
+    exit ${status}
+  fi
+fi
+
 # Build Final Target
-build_target ${RELEASE_TARGET} ${LOCAL_IMAGE_TAG}
+build_final ${RELEASE_STAGE} ${IMAGE_TAG}
