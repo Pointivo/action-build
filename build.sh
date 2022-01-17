258728d71ca1cb7e4f9c2741386ab73343754e98
Ryan Gartin <ryan@eagledevelopers.net>
2022-01-17 16:22:47 -0500

    Add optional variables; switch defaults to input

--- build.sh
+++ build.sh
@@ -6,32 +6,32 @@ if [ -z "${IMAGE_NAME}" ]; then
 fi
 
 # Script Variables / Defaults
-EXTRA_BUILD_ARGS=${EXTRA_BUILD_ARGS:-""}
-DOCKERFILE_DIRECTORY=${DOCKERFILE_DIRECTORY:-"./"}
-DOCKERFILE_NAME=${DOCKERFILE_NAME:-"Dockerfile"}
-CACHED_STAGES=${CACHED_STAGES:-"tooling,runtime,dependencies"}
-BUILD_STAGE=${BUILD_STAGE:-"build"}
+#EXTRA_BUILD_ARGS
+#DOCKERFILE_DIRECTORY
+#DOCKERFILE_NAME
+#CACHED_STAGES
+#BUILD_STAGE
+#PULL_IMAGES
+#RELEASE_TAG
 RELEASE_STAGE=${RELEASE_STAGE:-$IMAGE_NAME}
-PULL_IMAGES=${PULL_IMAGES:-""}
 CACHE_LAYER_PREFIX=${CACHE_LAYER_PREFIX:-$IMAGE_NAME}
 BUILD_IMAGE_NAME="${CACHE_LAYER_PREFIX}_${BUILD_STAGE}"
-dockerfile_directory=${DOCKERFILE_DIRECTORY:-./}
-dockerfile_name=${DOCKERFILE_NAME:-Dockerfile}
-DOCKERFILE="${dockerfile_directory%/}/${dockerfile_name}"
+DOCKERFILE="${DOCKERFILE_DIRECTORY%/}/${DOCKERFILE_NAME}"
+
 GIT_SHA_SHORT=$(git rev-parse --short HEAD)
-RELEASE_TAG=${RELEASE_TAG:-"snapshot"}
 REPOSITORY=${REPOSITORY:-$IMAGE_NAME}
 SEMVER=$(./version.sh -g)
 # Validate SEMVER
 semver diff ${SEMVER} ${SEMVER}
 FULL_SEMVER="${SEMVER}-${RELEASE_TAG}+GH${GITHUB_RUN_NUMBER}.${GIT_SHA_SHORT}"
 IMAGE_TAG="${SEMVER}_${RELEASE_TAG}_GH${GITHUB_RUN_NUMBER}.${GIT_SHA_SHORT}"
+
 echo "Dockerfile: ${DOCKERFILE}"
 echo "Git commit hash: ${GIT_SHA_SHORT}"
 echo "Stages to cache: ${CACHED_STAGES}"
 echo "build_args: ${build_args}"
-echo "semver: ${SEMVER}"
-echo "full semver: ${FULL_SEMVER}"
+echo "Semver: ${SEMVER}"
+echo "Full Semver: ${FULL_SEMVER}"
 # Save ENV variables for downstream Github Action steps
 echo "GIT_SHA_SHORT=${GIT_SHA_SHORT}" >> $GITHUB_ENV
 echo "SEMVER=${SEMVER}" >> $GITHUB_ENV
@@ -71,7 +71,7 @@ build_final() {
 }
 
 export_artifacts() {
-  echo "Exporting Test/Reports: ${ARTIFACT_PATHS}"
+  echo "Exporting Test/Reports: ${TEST_PATH}"
   mkdir -p ./build/artifacts
   id=$(docker create ${BUILD_IMAGE_NAME} /bin/sh)
   if [[ -n "${TEST_PATH}" ]]; then
